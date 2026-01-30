365f2fa74e51fed6920a7668953aa92ec2ef1ae3
“yasickogul” <yasickogul0705@gmail.comgit config --global user.name yasickogulgit config --global user.email yasickogul0705@gmail.com>
2026-01-30 14:22:44 +0530

    remove: delete example test file for Tamil converter

--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('https://tamil.changathi.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Online Tamil Converter | English Tamil Translation | Tamil Conversion/);
-});
